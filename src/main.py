22f65e9f0eccaccc8a3adc5e35f7fc7bff528cc2
hrokr <alex.rook@gmail.com>
2022-08-18 15:14:53 -0400

    Add: function for scheduling

--- src/main.py
+++ src/main.py
@@ -1,34 +1,48 @@
-import os
+import schedule
+import time
 from random import sample
 
-def enum_sayings(file):
-    with open (file) as f:
-        for (num, saying) in enumerate(f):
-            return num, saying
 
-
-def get_line_count(file)->int :
-    '''
+def get_line_count(file) -> int:
+    """
     Calculates the number of lines in file.
-    '''
- 
+    """
+
     with open(file) as f:
         line_count = sum(1 for _ in f)
     return line_count
 
 
-def gen_order (line_count: int):
-    print(f'the number of lines is {line_count}')
+def enum_sayings(file):
+    with open(file) as f:
+        for (num, saying) in enumerate(f):
+            return num, saying
 
+
+def gen_order(line_count: int):
     indexes = [i for i in range(line_count)]
     order = sample(indexes, len(indexes))
-    print (order)
 
 
+def get_saying():
+    pass
+
+
+def job():
+    print("I'm working...")
+
+    schedule.every(2).minutes.do(job)
+    # schedule.every().hour.do(job)
+    # schedule.every().day.at("10:30").do(job)
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    file = ('../data/CloneWarsSayings.txt')
+    file = "../data/CloneWarsSayings.txt"
 
     line_count = get_line_count(file)
     gen_order(line_count)
+    job(get_saying)
