00e4f14280e0101ae00c5692b0bfed9ee9be54bc
hrokr <alex.rook@gmail.com>
2022-08-30 23:08:31 -0400

    Add pathlib

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
-import os
-
+from oauthlib.oauth1.rfc5849.endpoints import access_token
+import pathlib
 # import schedule
 # jimport time
 import tweepy
 from random import sample, randint
 
 
-def get_keys():
-    keys = open("../tokens/jedi_tokens.txt", "r").read().splitlines()
-
-    return keys
+p = pathlib.Path('.')
 
 
 def get_line_count(file) -> int:
@@ -30,7 +27,7 @@ def get_todays_number():
     return todays_num
 
 
-def get_todays_saying(num_today):
+def generate_saying(num_today):
     """
     Generates saying for the day
     """
@@ -38,6 +35,20 @@ def get_todays_saying(num_today):
         for (num, saying) in enumerate(f):
             if num == num_today:
                 print(saying)
+
+
+def get_keys():
+    path = Path('../tokens/constants.py') = path.open()
+    return(keys)
+
+
+def parse_keys(keys):
+    api_key = API_key
+    api_key_secret = keys[1]
+    access_token = keys[2]
+    access_token_secret = keys[3]
+    print(api_key)
+    return(api_key, api_key_secret, access_token, access_token_secret)
 
 
 """
@@ -65,7 +76,7 @@ def job():
 
     schedule.every(2).minutes.do(job)
     # schedule.every().hour.do(job)
-    # schedule.every().day.at("10:30").do(job)
+    # schedule.every().day.at("10:30").do(job)lck
 
     while 1:
         schedule.run_pending()
@@ -75,21 +86,19 @@ def job():
 
 if __name__ == "__main__":
     file = "../data/CloneWarsSayings.txt"
-
     num_lines = get_line_count(file)
     print(f"There are {num_lines} sayings in the file")
-
     num_today = get_todays_number()
-    saying = get_todays_saying(num_today)
+    saying = generate_saying(num_today)
 
     keys = get_keys()
 
-    api_key = keys[0]
-    api_key_secret = keys[1]
-    access_token = keys[2]
-    access_token_secret = keys[3]
+    print('now to parse the keys')
+    for key in keys:
+        print(key)
 
-    print(keys[0])
+    api_key = parse_keys(keys)
+
 
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -98,3 +107,4 @@ if __name__ == "__main__":
     api.update_status(saying)
     # authenticate(api_key, api_key_secret, access_token, access_token_secret, saying)
     # post_saying(api, saying)
+
