32aecb3c828cac727489da6271182a15d09d9484
hrokr <alex.rook@gmail.com>
2022-09-04 11:34:07 -0400

    Add spelling to pre-commit

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ def post_saying(api_key, api_key_secret, access_token, access_token_secret, sayi
     """
     Posts saying
     """
-    print("now to parse the keys")
+    print("Done. Now to post the saying")
     auth = tweepy.OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
 
